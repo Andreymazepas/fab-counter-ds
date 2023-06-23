f2d3fb8372f763305f629dddab564e36cfc6bda0
Andrey Mazepas <andreymazepas@gmail.com>
2023-06-23 14:31:48 -0300

    overall structure

--- src/main.c
+++ src/main.c
@@ -1,26 +1,85 @@
-/*
- *  main.cpp
- *
- *  Hello World for Nintendo DS consoles
- *
- *  Created by Nicola Lamacchia, 2020
- *
- */
-
-#include <nds.h>
 #include <stdio.h>
+#include <nds.h>
+#include <filesystem.h>
+#include <nf_lib.h>
 
-int main()
-{
+#define MAX_LIFE 99
+#define MIN_LIFE 0
+
+int player1Life;
+int player2Life;
+
+void updateLife(int player, int change);
+void displayLifeTotals();
+void handleUserInput();
+void initializeGraphics();
+void displayBg();
+void displayButtons();
+
+int main() {
     consoleDemoInit();
+    initializeGraphics();
 
-    // Move the cursor to row 11 and column 9 and then print "Hello World"
-    printf("\x1b[12;10HO JAMES");
-    printf("\x1b[14;3H  ME VE UMA SALADA DE FRUTA");
+    // Initialize life totals
+    player1Life = 20;
+    player2Life = 20;
 
-    // Prevent Nintendo DS doesn't reboot upon program completion
-    for (;;)
-        ;
+    while (1) {  // Main game loop
+        displayBg();
+        displayButtons();
+        displayLifeTotals();
+        handleUserInput();
+        // ...
 
+        // Other game logic
+
+        // Probably reset?
+        // ...
+    }
     return 0;
+}
+
+void updateLife(int player, int change) {
+    // Update the life total for the specified player
+    if (player == 1) {
+        player1Life += change;
+        if (player1Life < MIN_LIFE)
+            player1Life = MIN_LIFE;
+        else if (player1Life > MAX_LIFE)
+            player1Life = MAX_LIFE;
+    } else if (player == 2) {
+        player2Life += change;
+        if (player2Life < MIN_LIFE)
+            player2Life = MIN_LIFE;
+        else if (player2Life > MAX_LIFE)
+            player2Life = MAX_LIFE;
+    }
+}
+
+void initializeGraphics() {
+    // Initialize the DS graphics system
+    // ...
+}
+
+void displayLifeTotals() {
+    // Display the life totals for both players on the DS screen
+    // Use the DS graphics system to render the life totals using custom font/sprites
+    // ...
+}
+
+void handleUserInput() {
+    // Handle user input for life changes
+    // Validate and process the input
+    // Update the life totals using the updateLife() function
+    // ...
+}
+
+void displayBg() {
+    // Display the background image on top/bottom screen
+    // ...
+}
+
+void displayButtons() {
+    // Display the buttons on the bottom screen
+    // ...
 }
